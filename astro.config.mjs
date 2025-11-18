b135b11fa711e687005ceec339e2af778a5a4b22
Itachposadas <posadas16.ap@gmail.com>
2025-11-18 00:31:04 -0600

    pages config
    otro mas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://itachposadas.github.io',
-  base: '/',
+  site: "https://itachposadas.github.io",
+  base: "/",
   integrations: [react()],
 });
