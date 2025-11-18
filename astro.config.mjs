110096e8ce1caed45ff0948c9b593605bc911a06
Itachposadas <posadas16.ap@gmail.com>
2025-11-17 23:00:07 -0600

    archivos faltantes
    verificar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+// https://astro.build/config
 export default defineConfig({
+  site: 'https://itachposadas.github.io',
+  base: '/',
   integrations: [react()],
 });
-
