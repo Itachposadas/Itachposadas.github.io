6678bbab3ca7854d6c18638d2223e6e9e0c22d5f
Itachposadas <posadas16.ap@gmail.com>
2025-11-18 00:24:51 -0600

    cambio
    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Itachposadas.github.io',
+  site: 'https://itachposadas.github.io',
   base: '/',
   integrations: [react()],
 });
