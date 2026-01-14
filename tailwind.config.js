9f3e14532e9b987a9d161d824b7d73851a2ad4bf
Itachposadas <posadas16.ap@gmail.com>
2026-01-14 10:47:35 -0600

    Sitio web prueba 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}",
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
   ],
-  darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-  ],
+  plugins: [],
 };
+
